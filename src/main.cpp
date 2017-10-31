ed02447fbe2b490abd91853da240e81948ff5de7
dsmith-projects <dss.inbox@gmail.com>
2017-10-30 22:55:16 -0500

    Estoy trabajando en el menú y algunas funcionalidades como agregar equipo y personas

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,54 @@
 
 using namespace std;
 
+vector<Equipo> vectorEquipos;
+
+void crearEquipo() {
+
+	string id;
+	cout << "Ingrese el id del equipo: ";
+	cin >> id;
+	//Equipo e = Equipo(id);
+	vectorEquipos.push_back(Equipo(id));
+}
+
+void crearMiembro() {
+	cout << "TODO > crear miembro" << endl;
+}
+
+void crearRequerimiento() {
+	cout << "TODO > crear requerimiento" << endl;
+}
+
+void crearTarea() {
+	cout << "TODO > crear tarea" << endl;
+}
+
+void asignarTareaAMiembro() {
+	cout << "TODO > asignar tarea a persona" << endl;
+}
+
+void mostrarEquipos() {
+	vector<Equipo>::iterator iteradorEquipo;
+
+	for(iteradorEquipo = vectorEquipos.begin(); iteradorEquipo != vectorEquipos.end(); ++iteradorEquipo) {
+			cout << (*iteradorEquipo).id << endl;
+	}
+}
+
+void desplegarMenu() {
+	cout << "\t*** MENU PRINCIPAL ***\n" << endl;
+	cout << "1. Crear un equipo" << endl;
+	cout << "2. Crear un miembro" << endl;
+	cout << "3. Crear un requerimiento" << endl;
+	cout << "4. Crear una tarea" << endl;
+	cout << "5. Asignar una tarea a un miembro de un equipo" << endl;
+	cout << "6. Mostrar equipos" << endl;
+	cout << "7. YYYY" << endl;
+	cout << "8. Salir" << endl;
+	cout << "\n" << endl;
+}
+
 int main()
 {
 	//    vector<Equipo> vecquipo;
@@ -55,6 +103,64 @@ int main()
 	//    std::cout<<"\n miembros "<<&e.miembros;
 	//    std::cout<<e.miembros[0]->nombre;
 	//    std::cout<<"\n miembros[0]"<<&e.miembros[0];
+
+	    /* Variables para la ejecucion del menu */
+	    	bool salir = false; // Bandera que indica el momento de parada
+	    	int opcion = 0; // Entero ingresado por el usuario para la seleccion una escoger una opcion del menu
+
+	    	while (!salir) {
+
+	    		desplegarMenu();
+	    		cout << "Ingrese una opcion del menu: ";
+	    		cin >> opcion;
+	    		cout << "\n" << endl;
+
+	    		switch (opcion) {
+				case 1: {
+					cout << ">>> CREAR UN NUEVO EQUIPO" << endl;
+					crearEquipo();
+					break;
+				}
+				case 2: {
+					cout << ">>> CREAR UN NUEVO MIEMBRO" << endl;
+					crearMiembro();
+					break;
+				}
+				case 3: {
+					cout << ">>> CREAR UN REQUERIMIENTO" << endl;
+					crearRequerimiento();
+					break;
+				}
+				case 4: {
+					cout << ">>> CREAR UNA NUEVA TAREA" << endl;
+					crearTarea();
+					break;
+				}
+				case 5: {
+					cout << ">>> ASIGNAR UNA TAREA A UN MIEMBRO DE UN EQUIPO" << endl;
+					asignarTareaAMiembro();
+					break;
+				}
+				case 6: {
+					cout << ">>> MOSTRAR EQUIPOS " << endl;
+					mostrarEquipos();
+					break;
+				}
+				case 7: { // en caso de necesitar más métodos
+					cout << "Prueba case 7: " << endl;
+					break;
+				}
+				case 8: {
+					salir = true;
+					cout << "Usted ha salido del programa\n" << endl;
+					break;
+				}
+				default: {
+					cout << "Opcion invalida. Intentelo nuevamente.\n" << endl;
+					//break;
+				}
+	    		}
+	    	}
 
     return 0;
 }
